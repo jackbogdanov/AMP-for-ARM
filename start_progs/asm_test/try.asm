c58eca6915505a6e99dc1749abd62222ede2e3df
Evgenij Bogdanov <gekabog@gmail.com>
2020-05-18 19:21:53 +0300

    test programms for begining

---
+++ start_progs/asm_test/try.asm
@@ -0,0 +1,9 @@
+
+_main:
+	.text
+	.retain
+    .retainrefs
+ 	 mov r0, pc
+	 mov r1, #2
+ 	 add r2, r1, r1
+
---
+++ start_progs/bare_metal_C_test/main.c
@@ -0,0 +1,11 @@
+
+ /*
+ * hello.c
+ */
+ #include <stdio.h>
+
+ int main(void) {
+
+     printf("Hello_world\n");
+     return 0;
+}
